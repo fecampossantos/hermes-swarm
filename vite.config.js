ab4d44ade8cd2ef50dd949d13ce1f0ddcd75533f
Felipe Santos <fe.campossantos@gmail.com>
2026-07-15 08:05:32 -0300

    added host

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: '0.0.0.0',
     proxy: {
       '/api': {
         target: 'http://100.65.187.88:9119',
